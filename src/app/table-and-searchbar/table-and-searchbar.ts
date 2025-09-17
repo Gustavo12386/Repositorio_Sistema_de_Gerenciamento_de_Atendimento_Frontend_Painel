909acc8d4f1f2c4b0f7f045d8555c5503b9f0b8f
Gustavo12386 <gucalderaro@hotmail.com>
2025-09-17 17:08:01 -0300

    adicionando tabela_dos_clientes e barra de pesquisa

---
+++ src/app/table-and-searchbar/table-and-searchbar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+
+@Component({
+  selector: 'app-table-and-searchbar',
+  imports: [TableModule],
+  templateUrl: './table-and-searchbar.html',
+  styleUrl: './table-and-searchbar.scss'
+})
+export class TableAndSearchbar {
+
+}
